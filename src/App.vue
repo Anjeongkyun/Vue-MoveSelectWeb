191becfc1cdd5f0c43a8e65cd19ae8760f76b4ef
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-04-20 15:29:11 +0900

    webMaking Repository first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id = "app">
+    <SignUpHeader></SignUpHeader>
+    <SignUpInput></SignUpInput>
+    <SignUpFooter></SignUpFooter>
+  </div>
+</template>
+
+<script>
+import SignUpHeader from './components/signup/SignUpHeader.vue'
+import SignUpInput from './components/signup/SignUpInput.vue'
+import SignUpFooter from './components/signup/SignUpFooter.vue'
+
+export default {
+    components: {
+    'SignUpHeader' : SignUpHeader,
+    'SignUpInput' : SignUpInput,
+    'SignUpFooter' : SignUpFooter,
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
